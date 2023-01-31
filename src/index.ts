d594d6ee5b8f5b90218fb474f83e94e7528b023a
Mani Movassagh <mani.mgh@gmail.com>
2023-01-31 01:35:38 +0100

    add json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import {PORT} from "./Constants/Constants";
+import {Request,Response} from "express";
 
 const app=express();
 
 
-app.use("/",(req,res)=>{
-    res.send("Hi Mani You made it")
+app.use("/",(req:Request,res:Response)=>{
+    res.json("Hi mani it is json from the backend ")
+
+
 })
 
 app.listen(PORT,()=>{
