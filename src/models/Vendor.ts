55a0e9b68d7b01fe3da4635fa9f5f5d6a39e86ac
Mani Movassagh <mani.mgh@gmail.com>
2023-02-14 14:26:37 +0100

    up to 19:4

--- src/models/Vendor.ts
+++ src/models/Vendor.ts
@@ -15,3 +15,26 @@ interface VendorDoc extends Document {
     rating: number;
     foods: any;
 }
+
+const VendorSchema = new Schema({
+
+    name: { type: String, require: true },
+    ownerName: { type: String, require: true },
+    foodType: { type: [String], require: true },
+    pincode: { type: String, require: true },
+    address: { type: String },
+    phone: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    salt: { type: String, require: true },
+    seviceAvailable: { type: Boolean },
+    coverImage: { type: [String] },
+    rating: { type: Number },
+    foods: [{
+        // @ts-ignore
+        type: mongoose.SchemaType.ObjectId,
+        ref: 'food'
+    }],
+
+
+}, { timestamps: true })
