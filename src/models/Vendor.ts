5cddd7bc64773511475fc4f4374aae0487993d2c
Mani Movassagh <mani.mgh@gmail.com>
2023-02-14 14:28:37 +0100

    typo

--- src/models/Vendor.ts
+++ src/models/Vendor.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
+import mongoose, {Document, Model, Schema} from "mongoose";
 
 interface VendorDoc extends Document {
     name: string;
@@ -18,23 +18,23 @@ interface VendorDoc extends Document {
 
 const VendorSchema = new Schema({
 
-    name: { type: String, require: true },
-    ownerName: { type: String, require: true },
-    foodType: { type: [String], require: true },
-    pincode: { type: String, require: true },
-    address: { type: String },
-    phone: { type: String, require: true },
-    email: { type: String, require: true },
-    password: { type: String, require: true },
-    salt: { type: String, require: true },
-    seviceAvailable: { type: Boolean },
-    coverImage: { type: [String] },
-    rating: { type: Number },
+    name: {type: String, require: true},
+    ownerName: {type: String, require: true},
+    foodType: {type: [String], require: true},
+    pincode: {type: String, require: true},
+    address: {type: String},
+    phone: {type: String, require: true},
+    email: {type: String, require: true},
+    password: {type: String, require: true},
+    salt: {type: String, require: true},
+    serviceAvailable: {type: Boolean},
+    coverImage: {type: [String]},
+    rating: {type: Number},
     foods: [{
-        // @ts-ignore
+        //@ts-ignore
         type: mongoose.SchemaType.ObjectId,
         ref: 'food'
     }],
 
 
-}, { timestamps: true })
+}, {timestamps: true})
